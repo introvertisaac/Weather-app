73b7fcf566ee308aa9b309db98dbbc09366f770d
ISAAC WAMBIRI <isaacwambiri254@gmail.com>
2023-09-25 14:22:38 +0300

    finalized api endpoints

--- main.js
+++ main.js
@@ -7,3 +7,46 @@ const cityInputElement = document.getElementById("city-input")
 const formElement = document.querySelector("form")
 
 
+
+formElement.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    const cityValue = cityInputElement.value;
+    getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue){
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`)
+
+        if(!response.ok) {
+            throw new Error("Network response was not found")
+        }
+
+        const data  = await response.json()
+
+        const temperature = Math.round(data.main.temp)
+        //        console.log(`The current temp in ${city} is: ` + `${temperature}`)
+
+        const description = data.weather[0].description
+
+        const icon = data.weather[0].icon
+
+        const details = [
+            `Feels like: ${Math.round(data.main.feels_like)}`,
+            `Humidity:${data.main.humidity}%`,
+            `Wind speed:${data.wind.speed}m/s`,
+        ]
+
+
+        weatherDataElement.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="icon">`;
+
+        weatherDataElement.querySelector(".temperature").textContent = `${temperature}C`;
+
+        weatherDataElement.querySelector(".description").textContent = description;
+
+        weatherDataElement.querySelector(".details").innerHTML = details.map((detail)=> `<div> ${detail} </div>`).join("");
+    } catch (error) {
+        
+    }
+
+}
