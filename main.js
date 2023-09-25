8e9e90a97f4acb1ccddb836565a0a3ae5d7419db
ISAAC WAMBIRI <isaacwambiri254@gmail.com>
2023-09-25 13:35:01 +0300

    called form elements

---
+++ main.js
@@ -0,0 +1,9 @@
+const apikey = "05b64fc018ea1e7a6dabc119e3cf33b9";
+
+
+const weatherDataElement = document.getElementById("weather-data")
+const cityInputElement = document.getElementById("city-input")
+
+const formElement = document.querySelector("form")
+
+
